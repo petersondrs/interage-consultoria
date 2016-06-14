d0800472ec8562d554431382899dba2364790426
Peterson Ramos <petersondrs@gmail.com>
2016-06-13 22:35:34 -0300

    Pagina flex con header e footer fixos e conteudo flexivel

--- wp-content/themes/interAge/interage.php
+++ wp-content/themes/interAge/interage.php
@@ -4,12 +4,12 @@ Template Name: A Interage
 */
 ?>
 <?php get_header(); ?>
-<section>
+<article>
     <?php $my_query = new WP_Query('p=216'); ?>
         <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
         <h1 class="page-title"><?php the_title(); ?></h1>
         <?php the_content (); ?>
     <?php endwhile; ?>
-</section>
+</article>
 
 <?php get_footer(); ?>
--- wp-content/themes/interAge/template-parts/content-single.php
+++ wp-content/themes/interAge/template-parts/content-single.php
@@ -12,11 +12,7 @@
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
-
-	<?php twentysixteen_excerpt(); ?>
-
-	<?php twentysixteen_post_thumbnail(); ?>
-
+	   <?php twentysixteen_post_thumbnail(); ?>
 	<div class="entry-content">
 		<?php
 			the_content();
@@ -35,19 +31,4 @@
 			}
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php twentysixteen_entry_meta(); ?>
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
-					get_the_title()
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
