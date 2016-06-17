bf9c3cd0214e8982ac9aa9b8ffb365e0067c8b8e
Peterson Ramos <petersondrs@gmail.com>
2016-06-16 22:04:20 -0300

    Correcao pagina de link que voltou a chamar o conteudo corretamente

--- wp-content/themes/interAge/links.php
+++ wp-content/themes/interAge/links.php
@@ -5,10 +5,10 @@ Template Name: Links
 ?>
 <?php get_header(); ?>
 <article class="list-style">
-    <?php $my_query = new WP_Query('category_name=links'); ?>
+    <?php $my_query = new WP_Query('p=222'); ?>
         <?php while ($my_query->have_posts()) : $my_query->the_post(); ?>
         <h1 class="page-title"><?php the_title(); ?></h1>
-        <?php echo excerpt('20'); ?>
+        <?php the_content (); ?>
     <?php endwhile; ?>
 </article>
 
